a93b60674e9d6a559e63cc445e915fd7f1d0f22c
Aruzhan Serikkan <akiii.fakiii@gmail.com>
2026-01-27 00:31:34 +0500

    casting of strings

--- python-basics/casting.py
+++ python-basics/casting.py
@@ -8,3 +8,8 @@ y = float(2.8)   # y will be 2.8
 z = float("3")   # z will be 3.0
 w = float("4.2") # w will be 4.2
 print(x,y,z,w)
+
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
+print(x,y,z)
